a423a4ceb87cd56a01094364854eb3cfb51505b0
Evan Anger <devandanger@gmail.com>
2021-11-11 01:20:23 -0500

    Updates

---
+++ Shared/ApiView.swift
@@ -0,0 +1,20 @@
+//
+//  ApiView.swift
+//  Sendgrid
+//
+//  Created by Evan Anger on 11/11/21.
+//
+
+import SwiftUI
+
+struct ApiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ApiView_Previews: PreviewProvider {
+    static var previews: some View {
+        ApiView()
+    }
+}
---
+++ Shared/Environments/ApiKeyStorage.swift
@@ -0,0 +1,8 @@
+//
+//  ApiKeyStorage.swift
+//  Sendgrid
+//
+//  Created by Evan Anger on 11/11/21.
+//
+
+import Foundation
