320bdc91507a9322ce1d59f51eb1582daec4ed43
Evan Anger <devandanger@gmail.com>
2021-11-11 00:24:03 -0500

    Initial Commit

---
+++ Shared/SendgridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendgridApp.swift
+//  Shared
+//
+//  Created by Evan Anger on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct SendgridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
